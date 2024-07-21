9cb38dbeab8cd2f6635eafcefc5d6e8310988e32
AlekhyaNam <alekhyanam@gmail.com>
2024-07-21 20:38:47 +0530

    Add files via upload
    A Calculator performed based on the Stack Expression Evalution.

---
+++ Calculator/Script.js
@@ -0,0 +1,266 @@
+var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+var mainDivContent=maindivs.innerHTML;
+
+
+
+var onediv=document.getElementsByClassName("num")[0];
+function one(){
+    // onediv.style.boxShadow="0px 0px 2px 2px grey";
+    var oneData=document.getElementsByClassName("num")[0].innerHTML;
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var oneContent=mainDivContent.concat(oneData);
+    maindivs.innerHTML=oneContent;
+    var bubble1=document.getElementsByClassName("num")[0];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var twodiv=document.getElementsByClassName("num")[1];
+function two(){
+    // twodiv.style.boxShadow="0px 0px 2px 2px grey";
+    var twoData=document.getElementsByClassName("num")[1].innerHTML;
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var twoContent=mainDivContent.concat(twoData);
+    console.log(twoContent);
+    maindivs.innerHTML=twoContent;
+    var bubble1=document.getElementsByClassName("num")[1];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var threediv=document.getElementsByClassName("num")[2];
+function three(){
+    // threediv.style.boxShadow="0px 0px 2px 2px grey";
+    var threeData=document.getElementsByClassName("num")[2].innerHTML;
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var threeContent=mainDivContent.concat(threeData);
+    maindivs.innerHTML=threeContent;
+    var bubble1=document.getElementsByClassName("num")[2];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var plusdiv=document.getElementsByClassName("num15")[0];
+function plus(){
+    // plusdiv.style.boxShadow="0px 0px 2px 2px grey"; 
+    var plusData=document.getElementsByClassName("num15")[0].innerHTML;
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var plusContent=mainDivContent.concat(plusData);
+    console.log(plusContent);
+    maindivs.innerHTML=plusContent;
+    var bubble1=document.getElementsByClassName("num15")[0];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var fourdiv=document.getElementsByClassName("num")[3];
+function four(){
+    // fourdiv.style.boxShadow="0px 0px 2px 2px grey";
+    var fourData=document.getElementsByClassName("num")[3].innerHTML;
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var fourContent=mainDivContent.concat(fourData);
+    maindivs.innerHTML=fourContent;
+    var bubble1=document.getElementsByClassName("num")[3];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var fivediv=document.getElementsByClassName("num")[4];
+function five(){
+    // fivediv.style.boxShadow="0px 0px 2px 2px grey";
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var fiveData=document.getElementsByClassName("num")[4].innerHTML;
+    var fiveContent=mainDivContent.concat(fiveData);
+    maindivs.innerHTML=fiveContent;
+    var bubble1=document.getElementsByClassName("num")[4];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var sixdiv=document.getElementsByClassName("num")[5];
+function six(){
+    // sixdiv.style.boxShadow="0px 0px 2px 2px grey";
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var sixData=document.getElementsByClassName("num")[5].innerHTML;
+    var sixContent=mainDivContent.concat(sixData);
+    maindivs.innerHTML=sixContent;
+    var bubble1=document.getElementsByClassName("num")[5];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var minusdiv=document.getElementsByClassName("num15")[1];
+function minus(){
+    // minusdiv.style.boxShadow="0px 0px 2px 2px grey";
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var minusData=document.getElementsByClassName("num15")[1].innerHTML;
+    var minusContent=mainDivContent.concat(minusData);
+    maindivs.innerHTML=minusContent;
+    var bubble1=document.getElementsByClassName("num15")[1];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var sevendiv=document.getElementsByClassName("num")[6];
+function seven(){
+    // sevendiv.style.boxShadow="0px 0px 2px 2px grey";
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var sevenData=document.getElementsByClassName("num")[6].innerHTML;
+    var sevenContent=mainDivContent.concat(sevenData);
+    maindivs.innerHTML=sevenContent;
+    var bubble1=document.getElementsByClassName("num")[6];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var eightdiv=document.getElementsByClassName("num")[7];
+function eight(){
+    // eightdiv.style.boxShadow="0px 0px 2px 2px grey";
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var eightData=document.getElementsByClassName("num")[7].innerHTML;
+    var eightContent=mainDivContent.concat(eightData);
+    maindivs.innerHTML=eightContent;
+    var bubble1=document.getElementsByClassName("num")[7];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var ninediv=document.getElementsByClassName("num")[8];
+function nine(){                                
+    // ninediv.style.boxShadow="0px 0px 2px 2px grey";
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var nineData=document.getElementsByClassName("num")[8].innerHTML;
+    var nineContent=mainDivContent.concat(nineData);
+    maindivs.innerHTML=nineContent;
+    var bubble1=document.getElementsByClassName("num")[8];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var mulitediv=document.getElementsByClassName("num15")[2];
+function mulite(){
+    // mulitediv.style.boxShadow="0px 0px 2px 2px grey";
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var muliteData=document.getElementsByClassName("num15")[2].innerHTML;
+    var muliteContent=mainDivContent.concat(muliteData);
+    maindivs.innerHTML=muliteContent;
+    var bubble1=document.getElementsByClassName("num15")[2];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var seediv=document.getElementsByClassName("numC")[0];
+console.log(seediv);
+function see(){
+    // seediv.style.boxShadow="0px 0px 2px 2px grey";
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    maindivs.innerHTML="";
+    console.log(mainDivContent);
+    // maindivs.innerHTML=seeContent;
+    var bubble1=document.getElementsByClassName("numC")[0];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var zerodiv=document.getElementsByClassName("num")[9];
+function zero(){
+    // zerodiv.style.boxShadow="0px 0px 2px 2px grey";
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var zeroData=document.getElementsByClassName("num")[9].innerHTML;
+    var zeroContent=mainDivContent.concat(zeroData);
+    maindivs.innerHTML=zeroContent;
+    var bubble1=document.getElementsByClassName("num")[9];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+var equaldiv=document.getElementsByClassName("num15")[3];
+function equal(){
+    // equaldiv.style.boxShadow="0px 0px 2px 2px grey";
+    var maindivsEqulas=document.getElementsByClassName("innerFirstDiv")[0];
+    let maindivsEqulasContent=maindivsEqulas.innerHTML;
+    let finalString=maindivsEqulasContent.split('');
+    console.log(finalString);
+    let stack=[];
+    let operations=[];
+    for(var i=0;i<finalString.length;i++){
+        if(finalString[i]>='0' && finalString[i]<='9'){
+            let temp="";
+            while(i<finalString.length && finalString[i]>='0' && finalString[i]<='9'){
+                temp+=finalString[i++];
+            }
+            let number=parseInt(temp,10);
+            console.log(number);
+            stack.push(number);
+            i--;
+        }
+        else if(finalString[i]==')'){
+            operations.push(finalString[i]);
+        }
+        else if(finalString[i]=='('){
+            while(operations[operations.length-1]!='('){
+                stack.push(performOPeration(operations.pop(),stack.pop(),stack.pop()));
+            }
+            operations.pop();
+        }
+        else if(finalString[i]=='+' || finalString[i]=='-' || finalString[i]=='*' || finalString[i]=='/'){
+            while(operations.length>0 && precedence(finalString[i],operations[operations.length-1])){
+                stack.push(performOPeration(operations.pop(),stack.pop(),stack.pop()));
+            }
+            operations.push(finalString[i]);
+        }
+    }
+    while(operations.length>0){
+        stack.push(performOPeration(operations.pop(),stack.pop(),stack.pop()));
+    }
+    console.log(stack[0]);
+    maindivsEqulas.innerHTML=stack.pop();
+    var bubble1=document.getElementsByClassName("num15")[3];
+    bubble1.style.animation="bubble 0.1s ease-in";
+}
+
+function precedence(operator1,operator2){
+    if(operator1=="(" || operator2==")"){
+        return false;
+    }
+    if((operator1 == '*' || operator1 == '/') &&
+    (operator2 == '+' || operator2 == '-')){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function performOPeration(op, b, a)
+    {
+        switch (op)
+        {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            if (b == 0)
+            {
+                document.write("Cannot divide by zero");
+            }
+            return parseInt(a / b, 10);
+        }
+        return 0;
+    }
+
+var divisiondiv=document.getElementsByClassName("num15")[4];
+function division(){
+    // divisiondiv.style.boxShadow="0px 0px 2px 2px grey";
+    var maindivs=document.getElementsByClassName("innerFirstDiv")[0];
+    var mainDivContent=maindivs.innerHTML;
+    var divisionData=document.getElementsByClassName("num15")[4].innerHTML;
+    var divisionContent=mainDivContent.concat(divisionData);
+    maindivs.innerHTML=divisionContent;
+    var bubble1=document.getElementsByClassName("num15")[4];
+    bubble1.style.animation="bubble 0.1s ease-in";
+
+}
